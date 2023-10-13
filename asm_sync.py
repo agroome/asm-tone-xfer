f2c49a7a7c9bd0d8037926349b0790c81c423431
Andy Groome <groome.andy@gmail.com>
2023-10-13 19:46:12 -0400

    refactored correlate funcs

--- asm_sync.py
+++ asm_sync.py
@@ -14,15 +14,15 @@ JOB_POLL = os.getenv('JOB_POLL', 5)
 
 ASM_TOKEN = os.getenv('ASM_TOKEN')
 
-inventory_columns = [
+inventory_columns = {
     'bd.ip_address', 'ports.ports', 'bd.original_hostname', 'bd.host', 'screenshot.finalurl', 
     'ports.services', 'ports.banners', 'ipgeo.asn'
-]
+}
 
-keyword_tags = ['Scan Name']
-keyword_tags = []
-inventory_tags = ['ipgeo.asn']
-# inventory_tags = []
+# keyword_tags = ['Scan Name']
+# keyword_tags = []
+# inventory_tags = {'ipgeo.asn'}
+# excluded_tags = []
 
 
 class ASM:
@@ -31,7 +31,12 @@ class ASM:
         self.limit = limit
         self.inventory_url = 'https://asm-demo.cloud.tenable.com/api/1.0/inventory' 
         self.tags_url = 'https://asm-demo.cloud.tenable.com/api/1.0/tags'
-        
+
+        tags = self.get_asm_tags()
+        self.tag_index = {f'bd.tag_{tag["id"]}_keyword': tag['name'] for tag in tags if tag['value_type'] == 'keyword'}
+        # create a list column names for the inventory request 
+        # self.tag_columns = [f'bd.tag_{tag["id"]}_keyword' for tag in tags if tag['value_type'] == 'keyword']
+        # make a lookup table to translate tag id to tag name
 
     def get_asm_tags(self) -> list[dict]:
         '''get a list of tags and return the id for tag with name'''
@@ -46,42 +51,21 @@ class ASM:
             filters = []
         response = requests.post(f'{self.inventory_url}?{url_params}', headers=headers, json=filters)
         return response.json()
-        
-    def create_tag_lookup(self):
-        '''create lookup table to translate keyword tag column names (tag_<id>_keyword) to readable name'''
-        headers = {'accept': 'application/json', 'Authorization': self.asm_token}
-        response = requests.get(self.tags_url, headers=headers)
-        asm_tags =  response.json()
-        # tag_tuples = [(f'bd.tag_{tag["id"]}_keyword', tag["name"]) for tag in asm_tags if tag['name'] in allow_tags]
-        return dict([(f'bd.tag_{tag["id"]}_keyword', tag["name"]) for tag in asm_tags if tag['name'] in keyword_tags])
 
-    def get_asm_inventory_records(self):    
-        # get ASM keyword tags to include in the inventory columns
-        tags = self.get_asm_tags()
-        tag_columns = [f'bd.tag_{tag["id"]}_keyword' for tag in tags if tag['value_type'] == 'keyword']
-    
-        # combine column names with tag names to include in the inventory query
-        column_list = ','.join(inventory_columns + tag_columns)
-        
-        a_records= [
+    def get_asm_inventory_records(self, limit=None):    
+        if limit is None:
+            limit = self.limit
+        record_filter= [
             {'column': 'bd.record_type', 'type': 'is', 'value': 'A'}
         ]
-        inventory = self.get_inventory(column_list, offset=0, limit=self.limit, filters=a_records) 
+        column_list = ','.join(inventory_columns | set(self.tag_index))
+        inventory = self.get_inventory(column_list, offset=0, limit=limit, filters=record_filter) 
 
         asm_data = pd.DataFrame.from_records(inventory['assets'])
-        columns = keyword_tags + inventory_tags + ['bd.ip_address']
-
-        # create lookup dict for renaming tag columns to readable tag names (from bd.tag_<ID>_keyword) 
-        tag_names = self.create_tag_lookup()
-        try:
-            asm_data = asm_data.rename(columns=tag_names)[columns]
-        except KeyError as e:
-            print(f'{str(e)} keyword tag not defined in ASM data')
-        return asm_data
+        return asm_data.rename(columns=self.tag_index)
 
 
 class TVM:
-
     def __init__(self, tio, source):
         self.tio = tio
         self.source = source
@@ -137,14 +121,15 @@ class TVM:
                 tag_name_lookup[value] = tag['uuid']
         return tag_name_lookup
 
-    def update_tags(self, asm_df):
-        ''' this needs to be reworked to apply more than one tag_value at a time'''
-        asm_attributes = keyword_tags + inventory_tags
-        for column in inventory_tags:
+    def update_tags(self, asm_df, tag_categories):
+        ''' update tags present in asm_df except for exluded_tags if specified'''
+        for column in tag_categories:
             # replace nan with empty string replace commas with spaces
             asm_df[column] = asm_df[column].fillna('').map(lambda x: x.replace(',', ' '))
 
-        for tag_category in asm_attributes:
+        for tag_category in tag_categories:
+            # if tag_category in excluded_tags:
+                # continue
             # create a 'non_empty' mask to filter out empty values
             non_empty = asm_df[tag_category] != ''
             tag_values = asm_df[non_empty][tag_category].unique()
@@ -161,34 +146,44 @@ class TVM:
                 self.tio.tags.assign(asset_uuids, [tag_uuid])
 
 
-def correlate_records(tvm, asm):
-        asm_df = asm.get_asm_inventory_records()
+def correlate_assets(tvm, asm, excluded_tags=None):
+    asm_df = asm.get_asm_inventory_records()
     
+    # check for existing assets in TVM
+    uuid_lookup = tvm.asset_ip_uuids()
+    # inject a tenable uuid for matching IP address in the ASM DataFrame
+    asm_df['uuid'] = asm_df['bd.ip_address'].map(lambda ip: uuid_lookup.get(ip))
+
+    # import discovered assets from ASM not in TVM
+    tvm.import_assets(asm_df)
+    return asm_df
+
+
+def correlate_tags(tvm, asm, asm_df, inventory_properties=None, excluded_tags=None):
+    if inventory_properties is None:
+        inventory_properties = []
+    if excluded_tags is None:
+        excluded_tags = []
+
+    # combine inventory properties with custom keyword tag names
+    tag_categories = (set(asm.tag_index.values()) | set(inventory_properties)) - set(excluded_tags)
+    print(f'keyword tags: {asm.tag_index}')
+    print(f'inventory properties: {inventory_properties}')
+    print(f'excluded tags: {excluded_tags}')
+    print(f'tag categories: {tag_categories}')
+
+    uuid_lookup = tvm.asset_ip_uuids()
         
-        # check for existing assets in TVM
-        uuid_lookup = tvm.asset_ip_uuids()
-        # inject a tenable uuid for matching IP address in the ASM DataFrame
-        asm_df['uuid'] = asm_df['bd.ip_address'].map(lambda ip: uuid_lookup.get(ip))
+    # inject a tenable uuid for matching IP address in the ASM DataFrame
+    asm_df['uuid'] = asm_df['bd.ip_address'].map(lambda ip: uuid_lookup.get(ip))
 
-        # import discovered assets from ASM not in TVM
-        tvm.import_assets(asm_df)
-
-        # allow time to process new import
-        time.sleep(600)
-
-        # refresh uuid table
-        uuid_lookup = tvm.asset_ip_uuids()
-        
-        # inject a tenable uuid for matching IP address in the ASM DataFrame
-        asm_df['uuid'] = asm_df['bd.ip_address'].map(lambda ip: uuid_lookup.get(ip))
-
-        tvm.update_tags(asm_df)
+    tvm.update_tags(asm_df, tag_categories)
 
     
-        # matching_uuids = asm_df[~asm_df['uuid'].isna()]['bd.ip_address']
-        # print(f'{len(matching_uuids)} ASM IPs with an asset uuid')
+    # matching_uuids = asm_df[~asm_df['uuid'].isna()]['bd.ip_address']
+    # print(f'{len(matching_uuids)} ASM IPs with an asset uuid')
 
-        return asm_df
+    # return asm_df
 
 
 def remove_commas(df: pd.DataFrame, columns: list):
@@ -200,10 +195,18 @@ def remove_commas(df: pd.DataFrame, columns: list):
 
 def main():
 
-        tvm = TVM(tio=TenableIO(), source='crowd')
+        tvm = TVM(tio=TenableIO(), source='spsource_new')
         asm = ASM(asm_token=ASM_TOKEN, limit=500)
 
-        correlate_records(tvm, asm)
+        asm_df = correlate_assets(tvm, asm)
+        print(f'ASM assets: {len(asm_df)}')
+        print(f'found in TIO: {len(asm_df[~asm_df.uuid.isna()])}')
+        print(f'not found in TIO: {len(asm_df[asm_df.uuid.isna()])}')
+
+        # allow time to process new import
+        # time.sleep(600)
+        correlate_tags(tvm, asm, asm_df, inventory_properties=['ipgeo.asn'], excluded_tags=['SP-ASM'])
+
 
 
 # def download_asm(): 
@@ -212,5 +215,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
-
